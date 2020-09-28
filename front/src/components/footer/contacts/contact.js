b4e1a2a53fc9284e7885dd47249df75954938129
YuzhikMariya <47949650+YuzhikMariya@users.noreply.github.com>
2020-09-28 14:10:15 +0300

    add adaptive design

--- front/src/components/footer/contacts/contact.js
+++ front/src/components/footer/contacts/contact.js
@@ -3,7 +3,7 @@ import s from  './contact.module.css';
 
 function Contact(props) {
   return (
-        <div class="contact">
+        <div class={s.contact}>
             <p className={s.header}>{props.header}</p>
             <p className={s.info}>{props.info}</p>
         </div>
